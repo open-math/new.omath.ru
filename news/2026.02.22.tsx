dc9bf90973d065c590014d8204e7f6d54122556a
Peter Radko <cmtvpetya@gmail.com>
2026-02-22 19:25:58 +0300

    Новость про статью "Выделение полного квадрата"

---
+++ news/2026.02.22.tsx
@@ -0,0 +1,10 @@
+const completingTheSquare = $CONTENT.basics.equations.quadratic.completingTheSquare;
+
+export default (
+  <>
+    <P>
+      Новая статья о <Ref to={completingTheSquare}>выделении полного квадрата</Ref> и решении любых квадратных
+      уравнений! С большим количеством примеров и наглядной визуализацией процесса.
+    </P>
+  </>
+);

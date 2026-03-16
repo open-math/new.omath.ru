597364e9b99d1a9d1c61a127b9576cd7d3b9b67a
Peter Radko <cmtvpetya@gmail.com>
2026-03-16 17:22:03 +0300

    Новость про новую страницу

---
+++ news/2026.03.16.tsx
@@ -0,0 +1,10 @@
+const zeroProductProperty = $CONTENT.basics.equations.zeroProductProperty;
+
+export default (
+  <>
+    <P>
+      Новая страница про <Ref to={zeroProductProperty}>метод нулевых множителей</Ref>, позволяющий быстро решать любые
+      уравнения, состоящие из набора множителей, произведение которых равно нулю.
+    </P>
+  </>
+);

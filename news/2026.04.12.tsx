e95087ee28a3d10dfe29aec4c2292528c59dc88c
Peter Radko <cmtvpetya@gmail.com>
2026-04-12 16:49:40 +0300

    Квадратные уравнения в реальной жизни

---
+++ news/2026.04.12.tsx
@@ -0,0 +1,10 @@
+const realWorld = $CONTENT.foundations.equations.quadratic.realWorld;
+
+export default (
+  <>
+    <P>
+      Страница с <Ref to={realWorld}>большой подборкой примеров</Ref> использования квадратных уравнений в реальной
+      жизни уже доступна! Она завершает большой цикл материалов по квадратным уравнениям.
+    </P>
+  </>
+);
